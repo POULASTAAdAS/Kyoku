77ed3d60b41392a6983b40beca29d79210d7c6e3
POULASTAAdAS <githubpoulastaa101@gmail.com>
2023-12-04 00:03:10 +0530

    finaly understood streaming concept

--- MusicStreamingApp/app/src/main/java/com/example/musicstreamingapp/MainActivity.kt
+++ MusicStreamingApp/app/src/main/java/com/example/musicstreamingapp/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.musicstreamingapp
 
-import android.media.MediaPlayer
 import android.os.Bundle
-import android.os.ParcelFileDescriptor
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.OptIn
@@ -13,90 +11,84 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
 import androidx.media3.common.util.Log
 import androidx.media3.common.util.UnstableApi
-import androidx.media3.exoplayer.SimpleExoPlayer
-import com.example.musicstreamingapp.temp.Api
+import androidx.media3.datasource.DataSource
+import androidx.media3.datasource.DefaultHttpDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.hls.HlsMediaSource
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+import androidx.media3.exoplayer.util.EventLogger
 import com.example.musicstreamingapp.ui.theme.MusicStreamingAppTheme
-import okhttp3.OkHttpClient
-import okhttp3.ResponseBody
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import java.io.FileOutputStream
-import kotlin.io.path.absolutePathString
 
 class MainActivity : ComponentActivity() {
     @OptIn(UnstableApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val client = OkHttpClient.Builder().build()
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl("http://192.168.0.105:8080/") // Update with your server's base URL
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .client(client)
-            .build()
-
-        val apiService = retrofit.create(Api::class.java)
-
-
         setContent {
             MusicStreamingAppTheme {
                 val context = LocalContext.current
 
-                val player = SimpleExoPlayer.Builder(context).build()
-//                    ExoPlayer.Builder(context)
-//                        .setMediaSourceFactory(
-//                            DefaultMediaSourceFactory(context)
-//                                .setLiveTargetOffsetMs(5000)
+                val dataSourceFactory: DataSource.Factory = DefaultHttpDataSource.Factory()
+
+                val hslMediaSource = HlsMediaSource
+                    .Factory(dataSourceFactory)
+                    .setAllowChunklessPreparation(true)
+                    .createMediaSource(
+                        MediaItem.Builder()
+                            .setMimeType(MimeTypes.APPLICATION_M3U8)
+                            .setUri("http://192.168.0.105:8080/song.m3u8")
+                            .setLiveConfiguration(
+                                MediaItem
+                                    .LiveConfiguration
+                                    .Builder()
+                                    .setMaxPlaybackSpeed(1.02f)
+                                    .build()
+                            )
+                            .build()
+                    )
+
+                val player = ExoPlayer.Builder(context)
+                    .setMediaSourceFactory(
+                        DefaultMediaSourceFactory(context)
+                            .setLiveTargetOffsetMs(5000)
+                    )
+                    .build()
+                player.setMediaSource(hslMediaSource)
+                player.prepare()
+                player.play()
+
+
+                player.addAnalyticsListener(EventLogger())
+
+
+//                val dataSourceFactory: DataSource.Factory = DefaultHttpDataSource.Factory()
+//
+//                val player = ExoPlayer.Builder(context)
+//                    .build()
+//                    .apply {
+//                        setMediaSource(
+//                            HlsMediaSource.Factory(dataSourceFactory)
+//                                .createMediaSource(
+//                                    MediaItem.Builder()
+//                                        .setUri("http://192.168.0.105:8080/song.m3u8")
+//                                        .build()
+//                                )
 //                        )
-//                        .build()
+//                        prepare()
+//                    }
 
-                val call: Call<ResponseBody> = apiService.getSong()
 
-                call.enqueue(
-                    object : Callback<ResponseBody> {
-                        override fun onResponse(
-                            call: Call<ResponseBody>,
-                            response: Response<ResponseBody>
-                        ) {
-                            val responseBody: ResponseBody? = response.body()
-
-                            responseBody?.let {
-                                Log.d("called", "responsebody $it")
-
-                                val inputStream = it.byteStream()
-
-                                val tempFile = kotlin.io.path.createTempFile(suffix = ".mp3")
-                                val outputStream = FileOutputStream(tempFile.toString())
-
-                                inputStream.use { input ->
-                                    outputStream.use { output ->
-                                        input.copyTo(output)
-                                    }
-                                }
-
-                                MediaPlayer().apply {
-                                    setDataSource(tempFile.absolutePathString())
-                                    prepareAsync()
-
-                                    setOnPreparedListener { player ->
-                                        player.start()
-                                    }
-                                }
-                            }
-                        }
-
-                        override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                            Log.d("called", "onFailure")
-                        }
-                    }
-                )
-
+//                val mediaItem = MediaItem.Builder()
+//                    .setUri("http://192.168.0.105:8080/song.m3u8")
+//                    .build()
+//
+//                player.prepare()
 
 
                 Surface(
@@ -108,7 +100,7 @@ class MainActivity : ComponentActivity() {
                         if (player.isPlaying) player.pause()
                         else {
                             player.play()
-                            Log.d("clicked", "dwjaiojdi")
+                            Log.d("clicked", "play clicked")
                         }
                     }) {
                         Text(text = "button")
--- MusicstreamingAppServer/src/main/kotlin/com/example/routes/Test.kt
+++ MusicstreamingAppServer/src/main/kotlin/com/example/routes/Test.kt
@@ -4,20 +4,104 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.utils.io.jvm.javaio.*
+import io.ktor.util.pipeline.*
 import java.io.File
 
 fun Route.getSong() {
-    get("/song") {
-        val file = File("F://songs//Aashiqui 2//Tum Hi Ho.mp3")
+//    get("/song") {
+////        call.respondFile(File("F://songs//Aashiqui 2//Tum_Hi_Ho.m3u8"))
+//        val playlistContent = buildString {
+//            appendLine("#EXTM3U")
+//            appendLine("#EXT-X-VERSION:3")
+//            appendLine("#EXT-X-TARGETDURATION:0")
+//            appendLine("#EXT-X-MEDIA-SEQUENCE:0")
+//            appendLine("#EXTINF:0.000011,")
+//            appendLine("http://192.168.0.105:8080/audio?index=1")
+//            appendLine("#EXT-X-ENDLIST")
+//        }
+//
+//        call.respondText(playlistContent, ContentType.parse("application/vnd.apple.mpegurl"))
+//    }
 
-        call.respondBytesWriter(contentType = ContentType.Audio.MPEG) {
-            file.inputStream().use {
-                it.copyTo(this)
-            }
+//    get("/audio{index}") {
+//        val index = call.parameters["index"] ?: return@get
+//
+//        println(index)
+//
+//        val filePath = "F://songs//Aashiqui 2//file0.m4a"
+//
+//        respondAudioSegment(File(filePath))
+//    }
+
+
+    get("/song.m3u8") {
+        val listOfIndex = listOf(
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
+        )
+
+        val playlistContent = StringBuilder()
+
+        val file = File("G://temp//Tum_Hi_Ho_Playlist.m3u8")
+
+        file.forEachLine {
+            if (it.endsWith(".m4a")) {
+                var dropSegment = it.drop(7)
+
+                dropSegment = if (dropSegment.first() in listOfIndex && dropSegment[1] in listOfIndex)
+                    "${dropSegment.first()}${dropSegment[1]}"
+                else dropSegment.first().toString()
+
+
+                playlistContent.appendLine("http://192.168.0.105:8080/audio?index=$dropSegment")
+            } else
+                playlistContent.appendLine(it)
         }
+
+        call.respondText(playlistContent.toString(), ContentType.parse("application/vnd.apple.mpegurl"))
+        println()
+        println()
+        println()
+        println()
+        println()
+    }
+
+    get("/audio{index}") {
+        val index = call.parameters["index"]!!
+        println(index)
+
+        val filePath = "G://temp////segment${index}ForTum_Hi_Ho.m4a" // segment26ForTum_Hi_Ho.m4a
+        respondAudioSegment(File(filePath))
+
+        println()
+        println()
+        println()
+        println()
+        println()
+        println()
+        println()
+        println()
+        println()
+        println()
+        println()
+        println()
+        println()
+        println()
     }
 }
+
+
+suspend fun PipelineContext<Unit, ApplicationCall>.respondAudioSegment(file: File) {
+    // Set response headers
+    call.response.header(HttpHeaders.ContentDisposition, "inline; filename=${file.name}")
+    call.response.header(HttpHeaders.ContentType, ContentType.Audio.MPEG.toString())
+
+    // Stream the file content
+//    call.respondOutputStream {
+//        file.inputStream().copyTo(this)
+//    }
+    call.respondFile(file)
+}
+
 
 fun Route.getCoverImage() {
     get("/coverImage") {
