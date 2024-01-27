a8930ad037d42b6e3ccf5d0954ff3a91e3c0a401
POULASTAAdAS <githubpoulastaa101@gmail.com>
2024-01-28 02:42:42 +0530

    modify metadta

--- initialScripts/selectSongsCopy.py
+++ initialScripts/selectSongsCopy.py
@@ -6,10 +6,12 @@ import subprocess
 
 count = 0
 
-copyFromDir = "F:/processed/"
+copyFromDir = "G:/Kyoku_2/"
 copyToDir = "E:/22-24/"
 
 listOfSongs = os.listdir(copyToDir)
+
+tag_list = []
 
 for song in listOfSongs:
     fileFromRootPath = os.path.join(copyToDir, song)
@@ -29,17 +31,32 @@ for song in listOfSongs:
             metadata = json.loads(result.stdout)
             tags = metadata.get('format', {}).get('tags', {})
 
-            if "Punjabi" in tags["album"]:
-                os.remove(fileFromRootPath)
-                print(fileFromRootPath)
+            genre = tags["genre"]
 
-        #     if not any(genre.lower() in {"punjabi", "bhojpuri"} for genre in tags["genre"]):
-        #         if int(tags["date"]) > 2021:
-        #             count += 1
-        #             copyToSongRootPath = os.path.join(copyToDir, song)
-        #             shutil.copy(fileFromRootPath, copyToSongRootPath)
-        #
-        #             print("copied: ", song, "count: ", count)
+            if genre not in tag_list:
+                tag_list.append(genre)
+                print(genre)
+
+            # if not any(genre.lower() in {"punjabi", "bhojpuri"} for genre in tags["genre"]):
+            #     if int(tags["date"]) > 2021:
+            #         count += 1
+            #         copyToSongRootPath = os.path.join(copyToDir, song)
+            #         shutil.copy(fileFromRootPath, copyToSongRootPath)
+            #
+            #         print("copied: ", song, "count: ", count)
 
     except Exception as e:
         continue
+
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+
+for i in tag_list:
+    print(i)
--- ktor-sample/src/main/kotlin/com/example/decodeStrings.kt
+++ ktor-sample/src/main/kotlin/com/example/decodeStrings.kt
@@ -1,23 +1,16 @@
 package com.example
 
 import com.upokecenter.cbor.CBORObject
-import java.math.BigInteger
 import java.nio.charset.StandardCharsets
-import java.security.KeyFactory
-import java.security.spec.*
 import java.util.*
 
 fun main() {
-    val publicKey =
-        "wuyXvmrde9hyjZBmNL5wDKSPRH_4Ivc6JS4qHPeQJVBdAAAAAOqbjWZNAR0hPOS2tIy1ddQAEBcMz-hOC1bFpP7bSfjydo-lAQIDJiABIVgg1bzte_gaJmcufiMhlpEaWkT1JJfOvMK4O0zPo4uAkl0iWCBr61JoNw5BDXSa7yVyG4SJJmVOVQRq9iS62M0a-3qODg=="
+    val clientDataJson =
+        "eyJ0eXBlIjoid2ViYXV0aG4uY3JlYXRlIiwiY2hhbGxlbmdlIjoiMVhHQXk2Y19zRmxOSUtZLUZxZ1IyRlkwMHB1N3AyZVhnUTM3bTJmcjNxbVZORUhIU0pXcTBUOVdzaVNacU1OTXZhT1VCcmJUTDhqbzdGak5MMGNXZ2ciLCJvcmlnaW4iOiJhbmRyb2lkOmFway1rZXktaGFzaDoxaFBoNVFhbE9MZ3ltZ1NWdnh2NVZVSnhwS1pQTkZBcHFzX1JIS3lSYldBIiwiYW5kcm9pZFBhY2thZ2VOYW1lIjoiY29tLnBvdWxhc3RhYS5wYXNzZWt5YXBwIn0"
 
-    val another =
-        "wuyXvmrde9hyjZBmNL5wDKSPRH_4Ivc6JS4qHPeQJVBdAAAAAOqbjWZNAR0hPOS2tIy1ddQAEBcMz-hOC1bFpP7bSfjydo-lAQIDJiABIVgg1bzte_gaJmcufiMhlpEaWkT1JJfOvMK4O0zPo4uAkl0iWCBr61JoNw5BDXSa7yVyG4SJJmVOVQRq9iS62M0a-3qODg=="
+    val dataString = clientDataJson.decodeBase64()
 
-    val byteArray = publicKey.b64Decode()
-
-    temp(byteArray)
-
+    println(dataString)
 //    attestationObject()
 }
 
@@ -49,13 +42,6 @@ private fun attestationObject() {
     val encodedPublicKey = byteArray.b64Encode()
 
     temp(byteArray)
-
-    // decode PublicKey
-    val previousByteArray = encodedPublicKey.b64Decode()
-    println("-----------------------------------------")
-    println(byteArray)
-    println(previousByteArray)
-    println(byteArray.contentEquals(previousByteArray))
 }
 
 private fun temp(byteArray: ByteArray) {
@@ -65,38 +51,8 @@ private fun temp(byteArray: ByteArray) {
     val hash = byteArray.copyOfRange(0, 32)
     val publicKeyByteArray = byteArray.copyOfRange(32, 64)
 
-    //    println("Hash: ${hash.joinToString("") { "%02x".format(it) }}")
-//    println("Public Key: ${publicKeyByteArray.joinToString("") { "%02x".format(it) }}")
-
-    val ecPoint = ECPoint(BigInteger(1, hash), BigInteger(1, publicKeyByteArray))
-
-    val ecParameterSpec = EllipticCurves.secp256k1().curve
-
-    val ecKeySpec = ECPublicKeySpec(ecPoint, ecParameterSpec)
-
-    val keyFactory = KeyFactory.getInstance("EC")
-    val ecPublicKey = keyFactory.generatePublic(ecKeySpec)
-
-    println("ecPublicKey: ${ecPublicKey}")
-}
-
-
-class EllipticCurves {
-    companion object {
-        fun secp256k1(): ECParams {
-            val p = BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16)
-            val a = BigInteger.ZERO
-            val b = BigInteger("7")
-            val n = BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", 16)
-            val gX = BigInteger("79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 16)
-            val gY = BigInteger("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8", 16)
-
-            val curveParams = ECParameterSpec(EllipticCurve(ECFieldFp(p), a, b), ECPoint(gX, gY), n, 1)
-            return ECParams(curveParams)
-        }
-    }
-
-    data class ECParams(val curve: ECParameterSpec)
+    println("Hash: ${hash.joinToString("") { "%02x".format(it) }}")
+    println("Public Key: ${publicKeyByteArray.joinToString("") { "%02x".format(it) }}")
 }
 
 
