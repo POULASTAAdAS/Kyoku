00b4a1413eb9341025b39836800332492d158daa
POULASTAAdAS <githubpoulastaa101@gmail.com>
2024-03-11 02:13:56 +0530

    Fixed SetBdate Screen

--- Kyoku/app/src/main/java/com/poulastaa/kyoku/presentation/screen/StartViewModel.kt
+++ Kyoku/app/src/main/java/com/poulastaa/kyoku/presentation/screen/StartViewModel.kt
@@ -23,7 +23,7 @@ class StartViewModel @Inject constructor(
 
     init { // read sign in state
         viewModelScope.launch {
-            _startDestination.value = Screens.GetSpotifyPlaylist.route
+            _startDestination.value = Screens.SetBirthDate.route
             dataStore.readSignedInState().collect {
 //                when (it) {
 //                    SignInStatus.AUTH.name -> _startDestination.value = Screens.Auth.route
