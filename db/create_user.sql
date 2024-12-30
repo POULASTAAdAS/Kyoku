8c53a8c1eb051638a20dce3b8224fe8de7771e1b
Poulastaa Das <poulastaadas2@gmail.com>
2024-12-30 23:56:56 +0530

    created database schema

--- db/create_user.sql
+++ db/create_user.sql
@@ -1,28 +1,82 @@
-create database  if not exists kyokuUser;
-use kyokuUser;
+CREATE DATABASE IF NOT EXISTS kyokuUser;
+USE kyokuUser;
 
-create table if not exists User(
-	id bigint primary key auto_increment,
-    userType varchar(7) not null,
-    email varchar(320) not null,
-    passwordHash varchar(700) not null,
-    profilePicUrl varchar(700) default null,
-    bDate date default null,
-    countryId int not null,
-    
-    unique key (userType , email)
+CREATE TABLE IF NOT EXISTS `User` (
+    id            BIGINT PRIMARY KEY AUTO_INCREMENT,
+    userType      VARCHAR(7)    NOT NULL,
+    username      VARCHAR(320)  NOT NULL,
+    email         VARCHAR(320)  NOT NULL,
+    passwordHash  VARCHAR(700)  NOT NULL,
+    profilePicUrl VARCHAR(700)  DEFAULT NULL,
+    bDate         DATE          DEFAULT NULL,
+    countryId     INT           NOT NULL,
+    UNIQUE KEY (userType, email)
 );
 
-create table if not exists UserJWTRelation(
-	userId bigint unique references `User`(id) on delete cascade,
-    refreshToken varchar(2000) not null,
-    
-    primary key(userId)
+CREATE TABLE IF NOT EXISTS UserJWTRelation (
+    userId       BIGINT UNIQUE REFERENCES `User` (id) ON DELETE CASCADE,
+    refreshToken VARCHAR(2000) NOT NULL
 );
 
-create table if not exists EmailVerification(
-	userId bigint unique references `User`(id) on delete cascade,
-    `status` bool not null default false
+CREATE TABLE IF NOT EXISTS UserPinned(
+	userId      BIGINT UNIQUE REFERENCES `User` (id) ON DELETE CASCADE,
+    otherId		BIGINT NOT NULL,
+    pinnedType	VARCHAR(9) NOT NULL,
+    PRIMARY KEY (userId, otherId, pinnedType)
 );
+
+CREATE TABLE IF NOT EXISTS UserPlaylist(
+	userId      BIGINT UNIQUE REFERENCES `User` (id) ON DELETE CASCADE,
+    playlistId	BIGINT NOT NULL,
+    PRIMARY KEY (userId, playlistId)
+);
+
+
+-- USER'S LIBRARY
+CREATE TABLE IF NOT EXISTS UserSavedPlaylist(
+	userId      BIGINT UNIQUE REFERENCES `User` (id) ON DELETE CASCADE,
+    playlistId	BIGINT NOT NULL,
+    PRIMARY KEY (userId, playlistId)
+);
+
+CREATE TABLE IF NOT EXISTS UserFavouriteSong(
+	userId	BIGINT UNIQUE REFERENCES `User` (id) ON DELETE CASCADE,
+    songId	BIGINT NOT NULL,
+    PRIMARY KEY (userId, songId)
+);
+
+CREATE TABLE IF NOT EXISTS UserSavedArtist(
+	userId	 	BIGINT UNIQUE REFERENCES `User` (id) ON DELETE CASCADE,
+    artistId	BIGINT NOT NULL,
+    PRIMARY KEY (userId, artistId)
+);
+
+CREATE TABLE IF NOT EXISTS UserSavedGenre(
+	userId	 	BIGINT UNIQUE REFERENCES `User` (id) ON DELETE CASCADE,
+    genreId	BIGINT NOT NULL,
+    PRIMARY KEY (userId, genreId)
+);
+
+CREATE TABLE IF NOT EXISTS UserSavedAlbum(
+	userId	BIGINT UNIQUE REFERENCES `User` (id) ON DELETE CASCADE,
+    albumId	BIGINT NOT NULL,
+    PRIMARY KEY (userId, albumId)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
