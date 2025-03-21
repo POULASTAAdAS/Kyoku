bc8cc5d7ad02b9042ec101348aca3471dbf68273
POULASTAAdAS <githubpoulastaa101@gmail.com>
2025-03-21 20:18:03 +0530

    constructing sync work manager

---
+++ Kyoku/main/data/src/main/java/com/poulastaa/main/data/repository/work/SyncArtistWorker.kt
@@ -0,0 +1,26 @@
+package com.poulastaa.main.data.repository.work
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.poulastaa.main.data.mapper.toWorkResult
+import com.poulastaa.main.domain.repository.work.WorkRepository
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+
+@HiltWorker
+class SyncPlaylistWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted params: WorkerParameters,
+    private val work: WorkRepository
+) : CoroutineWorker(context, params) {
+    override suspend fun doWork(): Result {
+        if (runAttemptCount >= 5) return Result.failure()
+
+        return when (val result = work.syncSavedPlaylist()) {
+            is com.poulastaa.core.domain.Result.Error -> result.error.toWorkResult()
+            is com.poulastaa.core.domain.Result.Success -> Result.success()
+        }
+    }
+}
---
+++ Kyoku/main/data/src/main/java/com/poulastaa/main/data/repository/work/SyncLibraryWorker.kt
@@ -0,0 +1,4 @@
+package com.poulastaa.main.data.repository.work
+
+class SyncLibraryWorker {
+}
