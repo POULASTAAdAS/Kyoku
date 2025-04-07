62ceee00a4151731548f1f6c27bf656ee6b2793c
Poulastaa Das <poulastaadas2@gmail.com>
2025-04-07 21:18:47 +0530

    added add module

---
+++ Kyoku/add/domain/src/main/java/com/poulastaa/add/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.poulastaa.add.domain
+
+class MyClass {
+}
---
+++ Kyoku/add/network/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.kyoku.android.library)
+    alias(libs.plugins.kyoku.okhttp.library)
+
+    alias(libs.plugins.kyoku.android.dagger)
+}
+
+android {
+    namespace = "com.poulastaa.add.network"
+}
+
+dependencies {
+    implementation(libs.okhttp.url.connection)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.gson)
+
+    implementation(projects.core.domain)
+    implementation(projects.add.domain)
+    implementation(projects.core.network)
+}
