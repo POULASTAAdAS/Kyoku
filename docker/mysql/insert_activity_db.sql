c5f8b21f97c46b4a5cee7f1716130afd9d0988af
Poulastaa Das <poulastaadas2@gmail.com>
2025-08-27 04:55:15 +0530

    populated song entry and relation with other table

--- docker/mysql/insert_activity_db.sql
+++
@@ -1,8 +0,0 @@
-USE ACTIVITY;
-
-INSERT IGNORE INTO UserPinnedType (id, `type`)
-VALUES
-    (1, "ALBUM"),
-    (2, "ARTIST"),
-    (3, "PLAYLIST"),
-    (4, "FAVORITE");
---
+++ kyoku-docker/mysql/insert_activity_db.sql
@@ -0,0 +1,8 @@
+USE ACTIVITY;
+
+INSERT IGNORE INTO UserPinnedType (id, `type`)
+VALUES
+    (1, "ALBUM"),
+    (2, "ARTIST"),
+    (3, "PLAYLIST"),
+    (4, "FAVORITE");
